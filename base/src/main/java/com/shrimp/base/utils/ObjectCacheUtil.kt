115e76b7cfa01ffc7a9c0ced6a0abe00ed1e0044
ChenXS <592485400@qq.com>
2021-11-15 18:11:56 +0800

    添加try/catch

--- base/src/main/java/com/shrimp/base/utils/ObjectCacheUtil.kt
+++ base/src/main/java/com/shrimp/base/utils/ObjectCacheUtil.kt
@@ -5,12 +5,8 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.withContext
 import java.io.IOException
 import kotlin.reflect.KClass
@@ -24,35 +20,46 @@ class ObjectCacheUtil(val context: Context) {
 
     suspend inline fun <reified T> read(key: String, crossinline func: (T) -> Unit) =
         coroutineScope {
-            context.dataStore.data
-                .catch {
-                    // 当读取数据遇到错误时，如果是 `IOException` 异常，发送一个 emptyPreferences 来重新使用
-                    // 但是如果是其他的异常，最好将它抛出去，不要隐藏问题
-                    if (it is IOException) {
-                        it.printStackTrace()
-                        emit(emptyPreferences())
-                    } else {
-                        throw it
-                    }
-                }
-                .map { value ->
-                    when (T::class) {
-                        Int::class -> value[intPreferencesKey(key)] ?: 0
-                        Double::class -> value[doublePreferencesKey(key)] ?: .0
-                        String::class -> value[stringPreferencesKey(key)] ?: ""
-                        Boolean::class -> value[booleanPreferencesKey(key)] ?: false
-                        Float::class -> value[floatPreferencesKey(key)] ?: .0f
-                        Long::class -> value[longPreferencesKey(key)] ?: 0L
-                        else -> {
+            try {
+                context.dataStore.data
+                    .catch {
+                        // 当读取数据遇到错误时，如果是 `IOException` 异常，发送一个 emptyPreferences 来重新使用
+                        // 但是如果是其他的异常，最好将它抛出去，不要隐藏问题
+                        if (it is IOException) {
+                            it.printStackTrace()
+                            emit(emptyPreferences())
+                        } else {
+                            throw it
                         }
                     }
-                }
-                .take(1) //限制只取一个，避免一直监听获取
-                .collect {
-                    withContext(Dispatchers.Main) {
-                        if (it is T) func.invoke(it as T)
+                    .map { value ->
+                        when (T::class) {
+                            Int::class -> value[intPreferencesKey(key)] ?: 0
+                            Double::class -> value[doublePreferencesKey(key)] ?: .0
+                            String::class -> value[stringPreferencesKey(key)] ?: ""
+                            Boolean::class -> value[booleanPreferencesKey(key)] ?: false
+                            Float::class -> value[floatPreferencesKey(key)] ?: .0f
+                            Long::class -> value[longPreferencesKey(key)] ?: 0L
+                            else -> {
+                            }
+                        }
                     }
+                    .take(1) //限制只取一个，避免一直监听获取
+                    .collect {
+                        withContext(Dispatchers.Main) {
+                            if (it is T) func.invoke(it as T)
+                        }
+                    }
+            } catch (e: Exception) {
+                when (T::class) {
+                    Int::class -> func.invoke(0 as T)
+                    Double::class -> func.invoke(.0 as T)
+                    String::class -> func.invoke("" as T)
+                    Boolean::class -> func.invoke(false as T)
+                    Float::class -> func.invoke(.0f as T)
+                    Long::class -> func.invoke(0L as T)
                 }
+            }
         }
 
     suspend fun save(key: String, value: Any) {
